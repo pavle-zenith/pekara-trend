2536efc3ebd7a2a2b42da9cbe7220003569f41b0
Branislav Stojanovic <bane.stojanovic00@gmail.com>
2024-07-15 18:39:58 +0200

    fix

--- public/app.js
+++ public/app.js
@@ -57,7 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
         .catch(error => console.error('Error:', error));
     });
 
-    const socket = new WebSocket('ws://35.159.161.213:80');
+    const socket = new WebSocket('ws://35.159.161.213:3000');
 
     socket.addEventListener("message", (event) => {
         const newOrder = JSON.parse(event.data);
